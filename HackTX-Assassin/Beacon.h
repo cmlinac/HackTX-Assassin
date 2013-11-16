c92e172c38c0409beeeb8fcb8c2c85e0176833a8
abouzek <alan.bouzek@gmail.com>
2013-11-16 00:37:26 -0600

    sign up and beacon api

---
+++ HackTX-Assassin/Beacon.h
@@ -0,0 +1,13 @@
+//
+//  Beacon.h
+//  HackTX-Assassin
+//
+//  Created by Alan Bouzek on 11/15/13.
+//  Copyright (c) 2013 cmlinac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Beacon : NSObject
+
+@end
---
+++ HackTX-Assassin/Beacon.m
@@ -0,0 +1,13 @@
+//
+//  Beacon.m
+//  HackTX-Assassin
+//
+//  Created by Alan Bouzek on 11/15/13.
+//  Copyright (c) 2013 cmlinac. All rights reserved.
+//
+
+#import "Beacon.h"
+
+@implementation Beacon
+
+@end
