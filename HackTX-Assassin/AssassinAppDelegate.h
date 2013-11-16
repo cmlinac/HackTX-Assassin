7a1b20d9e9181294dd47a5df96ef8a21cc044c4a
Chris <cmlinac@gmail.com>
2013-11-15 18:41:54 -0600

    added stuff

---
+++ HackTX-Assassin/AssassinAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AssassinAppDelegate.h
+//  HackTX-Assassin
+//
+//  Created by Chris Mlinac on 11/15/13.
+//  Copyright (c) 2013 cmlinac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssassinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackTX-Assassin/AssassinViewController.h
@@ -0,0 +1,13 @@
+//
+//  AssassinViewController.h
+//  HackTX-Assassin
+//
+//  Created by Chris Mlinac on 11/15/13.
+//  Copyright (c) 2013 cmlinac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssassinViewController : UIViewController
+
+@end
